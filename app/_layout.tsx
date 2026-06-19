6989a6225bc6d7f09fced238e518a11fa70fb472
ehdgus02 <ehdgus02kk@naver.com>
2026-06-19 11:39:57 +0900

    테마 적용 완료

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,23 @@
 import { Slot } from "expo-router";
 import "../styles/global.css";
+import { useThemeStore } from "@/stores/theme/useThemeStore";
+import { useColorScheme } from "nativewind";
+import { View } from "react-native";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-    return <Slot />;
+    const { theme } = useThemeStore();
+
+    // 앱에서 라이트모드와 다크모드를 적용하기 위한 기능을 호출
+    const { setColorScheme } = useColorScheme();
+
+    useEffect(() => {
+        setColorScheme(theme);
+    }, [theme, setColorScheme]);
+
+    return (
+        <View className={theme}>
+            <Slot />
+        </View>
+    );
 }

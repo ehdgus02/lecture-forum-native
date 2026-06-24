a1687d9b60e3aa279a2f81acb54c6bb2a5b82422
ehdgus02 <ehdgus02kk@naver.com>
2026-06-24 15:53:19 +0900

    AdminCategoryListPage 작성 완료

---
+++ api/admin/adminCategoryApi.ts
@@ -0,0 +1,17 @@
+import { Category } from "@/types/category";
+import axiosInstance from "@/api/axiosInstance";
+
+const getCategoryList = async (): Promise<Category[]> => {
+    const response = await axiosInstance.get("/admin/category/list");
+    return response.data.data;
+};
+
+const toggleCategoryStatus = async (id: number): Promise<Category> => {
+    const response = await axiosInstance.patch(`/admin/category/${id}/status`);
+    return response.data.data;
+};
+
+export default {
+    getCategoryList,
+    toggleCategoryStatus,
+};
